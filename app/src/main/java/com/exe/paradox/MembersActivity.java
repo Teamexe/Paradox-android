4030bbdebbf4c6f45ca6e498d8f41f3b99238646
okatticus <apporva.hm@gmail.com>
2018-03-29 03:12:05 +0530

    decorating members page

--- app/src/main/java/com/exe/paradox/MembersActivity.java
+++ app/src/main/java/com/exe/paradox/MembersActivity.java
@@ -15,34 +15,7 @@ public class MembersActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
-        final FoldingCell fc = findViewById(R.id.folding_cell);
-        fc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fc.toggle(false);
-            }
-        });
-        final FoldingCell fc1 = findViewById(R.id.folding_cell1);
-        fc1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fc1.toggle(false);
-            }
-        });
-        final FoldingCell fc2 = findViewById(R.id.folding_cell2);
-        fc2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fc2.toggle(false);
-            }
-        });
-        final FoldingCell fc3 = findViewById(R.id.folding_cell3);
-        fc3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fc3.toggle(false);
-            }
-        });
-    }
+
+}
 
 }
